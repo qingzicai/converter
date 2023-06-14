61b5956c7cd4f4c55cb3c8a45fc7a7de77184593
ivy <dllmwl08@gmail.com>
2023-06-14 10:05:16 -0400

    fixed bugs and changed the linked src to ./index.js

---  index.js
+++  index.js
@@ -8,70 +8,75 @@ const formula = document.querySelector("#formula");
 // event listener that fetches the corresponding options and update the unit dropdown menus according to the changes of the category.
 category.addEventListener("change", (evt) => {
     evt.preventDefault();
-    const selectedCategory = category.value;
-    const options = passArray(selectedCategory);
-    addUnitsByCategory(selectedCategory, options);
+    const unitOptions = passArray(category.value);
+    addUnitsByCategory(unitOptions);
 });
 
 // populate the unit1 and unit2 dropdown menus with the available options based on the selected category
-const addUnitsByCategory = (selectedCategory, options) => {
+function addUnitsByCategory(unitOptions) {
     let optionsHTML = "";
-    for (let i = 0; i < options.length; i++) {
-    optionsHTML += `<option>${options[i]}</option>`;
+    for (let i = 0; i < unitOptions.length; i++) {
+        optionsHTML += `<option>${unitOptions[i]}</option>`;
     }
     unit1.innerHTML = optionsHTML;
     unit2.innerHTML = optionsHTML;
-};
-
-  // takes the selectedCategory as a parameter and uses a switch statement to determine which array of options to return.
-function passArray(selectedCategory) {
-    switch (selectedCategory) {
-    case "length":
-        return ["Kilometer", "Meter", "Mile"];
-    case "digital-storage":
-        return ["Byte", "Kilobyte"];
-    case "fuel-economy":
-        return ["Kilometer per liter", "Miles per gallon"];
-    default:
-        return [];
-    }
 }
 
-
+// takes the selectedCategory as a parameter and uses a switch statement to determine which array of options to return.
+function passArray(selectedCategory) {
+    switch (category.value) {
+        case "length":
+        return ["Kilometer", "Meter", "Mile"];
+        case "digital-storage":
+        return ["Byte", "Kilobyte"];
+        case "fuel-economy":
+        return ["Kilometer_per_liter", "Miles_per_gallon"];
+        default:
+        return [];
+    }
+    }
 
 const conversionFactorList = {
     Kilometer: {
         Meter: 1000,
-        Mile: 0.621371
+        Mile: 0.621371,
+        Kilometer: 1
     },
     Meter: {
         Kilometer: 0.001,
-        Mile: 0.000621371
+        Mile: 0.000621371,
+        Meter: 1
     },
     Mile: {
         Kilometer: 1.60934,
-        Meter: 1609.34
+        Meter: 1609.34,
+        Mile: 1
     },
     Kilometer_per_liter: {
-        Kilometer_per_gallon: 2.352
+        Kilometer_per_gallon: 2.352,
+        Kilometer_per_liter: 1
     },
     Kilometer_per_gallon: {
-        Kilometer_per_liter: 0.425144
+        Kilometer_per_liter: 0.425144,
+        Kilometer_per_gallon: 1
     },
     Byte: {
-        Kilobyte: 0.001
+        Kilobyte: 0.001,
+        Byte: 1
     },
     Kilobyte: {
-        Byte: 1000
+        Byte: 1000,
+        Kilobyte: 1
     }
 };
 
+//if unit1 and unit2 are not empty, return conversion factor
 const conversionFactor = (inputUnit1, inputUnit2) => {
     let metricUnit1 = inputUnit1;
     let metricUnit2 = inputUnit2;
-    
+
     if (conversionFactorList[metricUnit1] && conversionFactorList[metricUnit2]) {
-    return conversionFactorList[metricUnit1][metricUnit2];
+        return conversionFactorList[metricUnit1][metricUnit2];
     }
 
     return 1;
@@ -81,20 +86,6 @@ unit1.addEventListener("change", handleUnitChange);
 unit2.addEventListener("change", handleUnitChange);
 input1.addEventListener("input", () => handleInputChange(input1, input2));
 input2.addEventListener("input", () => handleInputChange(input2, input1));
-
-// updating the otherInput value when the units are changed without modifying the input values.
-function handleUnitChange() {
-    const metricUnit1 = unit1.value;
-    const metricUnit2 = unit2.value;
-    // get the current input value
-    const inputValue = input1.value;
-
-    if (inputValue === "") {
-        input2.value = "";
-    } else {
-    convert(metricUnit1, metricUnit2, inputValue, input1, input2);
-    }
-}
 
 // handling the input changes，performs conversion and updates values of both inputs accordingly.
 function handleInputChange(input, otherInput) {
@@ -106,7 +97,21 @@ function handleInputChange(input, otherInput) {
         input.value = "";
         otherInput.value = "";
     } else {
-    convert(metricUnit1, metricUnit2, value, input, otherInput);
+        convert(metricUnit1, metricUnit2, value, input, otherInput);
+    }
+}
+
+// updating the otherInput value when the units are changed without modifying the input values.
+function handleUnitChange() {
+    const metricUnit1 = unit1.value;
+    const metricUnit2 = unit2.value;
+    // get the current input value
+    const inputValue = input1.value;
+
+    if (inputValue === "") {
+        input2.value = "";
+    } else {
+        convert(metricUnit1, metricUnit2, inputValue, input1, input2);
     }
 }
 
@@ -114,17 +119,44 @@ function handleInputChange(input, otherInput) {
 function convert(metricUnit1, metricUnit2, inputValue, input, otherInput) {
     const factor = conversionFactor(metricUnit1, metricUnit2);
 
-    if (input === input1) {
-      otherInput.value = inputValue * factor;
-    } else if (input === input2) {
-    otherInput.value = inputValue / factor;
+    if (input.value === input1.value) {
+        otherInput.value = inputValue * factor;
+    } else if (input.value === input2.value) {
+        otherInput.value = inputValue / factor;
     }
     formula.innerText = `The conversion factor of transferring ${metricUnit1} to ${metricUnit2} is ${factor}.`;
 }
 
 
-// if (input.value === input1.value) {
-//     otherInput.value = inputValue * factor;
-//   } else if (input.value === input2.value) {
-//     otherInput.value = inputValue / factor;
-//   }
+
+// // populate the unit1 and unit2 dropdown menus with the available options based on the selected category
+// function addUnitsByCategory (unitOptions) {
+//     const selectedCategory = category.value; 
+//     const unitOptions = passArray(selectedCategory);
+//     let optionsHTML = "";
+//     for (let i = 0; i < unitOptions.length; i++) {
+//     optionsHTML += `<option>${unitOptions[i]}</option>`;
+//     }
+//     unit1.innerHTML = optionsHTML;
+//     unit2.innerHTML = optionsHTML;
+// };
+
+// // takes the selectedCategory as a parameter and uses a switch statement to determine which array of options to return.
+// function passArray(selectedCategory) {
+//     const selectedCategory = category.value;
+//     switch (selectedCategory) {
+//     case "length":
+//         return ["Kilometer", "Meter", "Mile"];
+//     case "digital-storage":
+//         return ["Byte", "Kilobyte"];
+//     case "fuel-economy":
+//         return ["Kilometer_per_liter", "Miles_per_gallon"];
+//     default:
+//         return [];
+//     }
+// }
+
+
+
+
+
